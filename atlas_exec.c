a0ff71bc830fcd4526ca0d8862d199a2f0d7e403
Phuc-Dao <tidaotidao@yahoo.com>
2018-12-18 00:53:21 -0800

    preserve LD_PRELOAD variable

--- atlas_exec.c
+++ atlas_exec.c
@@ -15,3 +15,55 @@ struct atlas_backend_ctx {
     const char* cmd;
     char* envp[];
 };
+
+static void exec_destroy_ctx(atlas_backend_ctx_t* ctx)
+{
+    free((void*)ctx->cmd);
+    char** tmp;
+    for (tmp = ctx->envp; tmp && *tmp; tmp++)
+    {
+	free (*tmp);
+    }
+    free(ctx);
+}
+
+extern char** environ;
+
+#define LD_PRELOAD_ENV "LD_PRELOAD="
+
+static atlas_backend_ctx_t* exec_create_ctx (const char* cmd){
+    int n_envp = 0;
+    char** envp = enviorn;
+    assert (envp);
+    if(envp) for (; *envp; envp++; n_envp++);
+    
+    size_t const ctx ctx_size = sizeof(atlas_backend_ctx_t) + 
+        (n_envp + 1) * sozepf(char*);
+
+    atlas_backend_ctx_t* ret = calloc (1, ctx_size);
+    if (!ret) return NULL;
+    ret->cmd = strdup(cmd);
+    
+    envp = environ;
+    int i = 0;
+    
+    if(envp){
+        for(; *envp; envp++)
+        {
+	    if(!strncmp(*envp, LD_PRELOAD_ENV, sizeof(LD_PRELOAD_ENV) - 1))
+	    {
+	        continue;
+	    }
+
+	}
+    }
+
+	return ret;
+}
+
+
+
+
+
+
+
